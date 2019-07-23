036fe3c2c1839a17a6ae8a3ef8a54a68292b90eb
Dudi Levy <dudi@elv8.co>
2019-07-23 18:22:53 +0300

    Updated and cleanup of bucketAPI code

--- py365/resources/bucket_api.py
+++ py365/resources/bucket_api.py
@@ -1,9 +1,15 @@
+from typing import Optional
 from ._base_resource import BaseResource
 from ._child_resource import ChildResource
 from py365 import data
 
 
 class BucketAPI(ChildResource):
+    """
+    https://docs.microsoft.com/en-us/graph/api/resources/plannerbucket?view=graph-rest-1.0
+
+    The plannerBucket resource represents a bucket (or "custom column") for tasks in a plan in Office 365
+    """
 
     def __init__(self, plannerAPI: BaseResource, bucketID: str = None, bucket: data.PlannerBucket = None):
         if bucketID:
@@ -11,10 +17,56 @@ class BucketAPI(ChildResource):
         elif bucket and bucket.id:
             self.bucketID = bucket.id
         else:
-            raise ValueError("Either bucketId or bucket must have a valid value")
+            self.bucketID = ""
         super().__init__(baseAPI=plannerAPI, edgeMid=f"/buckets/{self.bucketID}")
 
+    def createBucket(self, newBucket: data.PlannerBucket) -> Optional[data.PlannerBucket]:
+        """
+        https://docs.microsoft.com/en-us/graph/api/planner-post-buckets?view=graph-rest-1.0&tabs=http
+
+        Use this API to create a new plannerBucket
+        :param newBucket: the new bucket data
+        :type newBucket: data.PlannerBucket
+        :return: the bucket object created
+        :rtype: data.PlannerBucket
+        """
+        json = newBucket.json
+        response = self.postAPI(json=json)
+        if response.ok:
+            respJson = response.json()
+            bucket = data.PlannerBucket()
+            bucket.fromResponse(data=respJson)
+            return bucket
+        else:
+            print(f'Request Error{response.text}')
+            return None
+
+    def getBucket(self) -> Optional[data.PlannerBucket]:
+        """
+        https://docs.microsoft.com/en-us/graph/api/plannerbucket-get?view=graph-rest-1.0&tabs=http
+
+        Retrieve the properties and relationships of plannerBucket object.
+        :return: The requested bucket object
+        :rtype: data.PlannerBucket
+        """
+        response = self.getAPI()
+        if response.ok:
+            respJson = response.json()
+            bucket = data.PlannerBucket()
+            bucket.fromResponse(data=respJson)
+            return bucket
+        else:
+            print(f'Request Error{response.text}')
+            return None
+
     def listTasks(self) -> [data.PlannerTask]:
+        """
+        https://docs.microsoft.com/en-us/graph/api/plannerbucket-list-tasks?view=graph-rest-1.0&tabs=http
+
+        Retrieve a list of plannerTask objects associated to a plannerBucket object
+        :return: list of tasks
+        :rtype: [data.PlannerTask]
+        """
         tasks: [data.PlannerTask] = []
         response = self.getAPI(edgeEnd="/tasks")
         if response.ok:
@@ -26,3 +78,36 @@ class BucketAPI(ChildResource):
         else:
             print(f'Request Error{response.text}')
         return tasks
+
+    def updateBucket(self, updateBucket: data.PlannerBucket) -> Optional[data.PlannerBucket]:
+        """
+        https://docs.microsoft.com/en-us/graph/api/plannerbucket-update?view=graph-rest-1.0&tabs=http
+
+        Update the properties of plannerBucket object
+        :param updateBucket: the information to update
+        :type updateBucket: data.PlannerBucket
+        :return: the updated bucket object data
+        :rtype: data.PlannerBucket
+        """
+        raise NotImplementedError("Method not yet Implemented")  # Need to implement ETag first...
+        updateBucket.id = None  # remove bucket ID to avoid issues with different buckets id in request and response
+        json = updateBucket.json
+        response = self.patchAPI(json=json)
+        if response.ok:
+            respJson = response.json()
+            bucket = data.PlannerBucket()
+            bucket.fromResponse(data=respJson)
+            return bucket
+        else:
+            print(f'Request Error {response.text}')
+            return None
+
+    def deleteBucket(self):
+        """
+        https://docs.microsoft.com/en-us/graph/api/plannerbucket-delete?view=graph-rest-1.0&tabs=http
+
+        Delete plannerBucket
+        :return: none
+        :rtype: none
+        """
+        raise NotImplementedError("Method not yet Implemented")
--- py365/resources/planner.py
+++ py365/resources/planner.py
@@ -1,6 +1,5 @@
 # API Reference
 # https://docs.microsoft.com/en-us/graph/api/resources/planner-overview?view=graph-rest-1.0
-from typing import Optional
 from py365 import auth, data
 from ._base_resource import BaseResource
 from .bucket_api import BucketAPI
@@ -15,29 +14,10 @@ class Planner(BaseResource):
     def __init__(self, connection: auth.AppConnection):
         super().__init__(connection, edgeBase='/planner')
 
-    def plans(self, planID: str = None, plan: data.PlannerPlan = None) -> PlanAPI:
+    def plan(self, planID: str = None, plan: data.PlannerPlan = None) -> PlanAPI:
         plansAPI = PlanAPI(plannerAPI=self, planID=planID, plan=plan)
         return plansAPI
 
     def bucket(self, bucketID: str = None, bucket: data.PlannerBucket = None) -> BucketAPI:
         bucketAPI = BucketAPI(plannerAPI=self, bucketID=bucketID, bucket=bucket)
         return bucketAPI
-
-    def createBucket(self, bucket: data.PlannerBucket) -> Optional[data.PlannerBucket]:
-        """
-        https://docs.microsoft.com/en-us/graph/api/planner-post-buckets?view=graph-rest-1.0&tabs=http
-        :param bucket:
-        :type bucket:
-        :return:
-        :rtype:
-        """
-        json = bucket.json
-        response = self.postAPI("/buckets", json=json)
-        if response.ok:
-            respJson = response.json()
-            bucket = data.PlannerBucket()
-            bucket.fromResponse(data=respJson)
-            return bucket
-        else:
-            print(f'Request Error{response.text}')
-            return None
